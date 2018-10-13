b1c0fc4344a9a7b30555213402fa8dba76a391c4
Bruno Santana Alves <bruno.sant.a@gmail.com>
2018-10-13 11:51:46 -0300

    first commit

---
+++ lib/fft.ex
@@ -0,0 +1,18 @@
+defmodule FFT do
+  @moduledoc """
+  Documentation for FFT.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FFT.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fft_test.exs
@@ -0,0 +1,8 @@
+defmodule FFTTest do
+  use ExUnit.Case
+  doctest FFT
+
+  test "greets the world" do
+    assert FFT.hello() == :world
+  end
+end
