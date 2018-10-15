cec633a9e2cc4f00d859b175d68a884e61c1124c
Bruno Santana Alves <bruno.sant.a@gmail.com>
2018-10-15 11:36:36 -0300

    hex docs fix

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule FFT.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       name: "FFT",
       source_url: "https://github.com/brunosantanaa/fft"
@@ -38,8 +40,7 @@ defmodule FFT.MixProject do
       # This option is only needed when you don't want to use the OTP application name
       name: "fft",
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w( lib .formatter.exs mix.exs README* ),
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/brunosantanaa/fft"}
     ]
