15ddb0f327867fe2c65cff1f472bdd68e96de19b
Bruno Santana Alves <bruno.sant.a@gmail.com>
2018-10-24 14:51:09 -0300

    New version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FFT.MixProject do
   def project do
     [
       app: :fft,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
